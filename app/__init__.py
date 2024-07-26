2a33b543c81188e0efe8687f19771c903424671d
Suryanshsinh Sisodiya <suryanshsinhsisodiya@gmail.com>
2024-07-26 18:21:49 +0530

    update

--- app/__init__.py
+++ app/__init__.py
@@ -16,3 +16,12 @@ def make_celery(app):
 
     celery.Task = ContextTask
     return celery
+
+def create_app():
+    app = Flask(__name__)
+    # Configure your app
+    # app.config.from_object('config.Config')
+    # Register blueprints
+    # from .main import bp as main_bp
+    # app.register_blueprint(main_bp)
+    return app
