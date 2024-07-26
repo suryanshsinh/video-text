1cb598317043255883279164a5b6086992fab213
Suryanshsinh Sisodiya <suryanshsinhsisodiya@gmail.com>
2024-07-26 17:46:35 +0530

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
+import os
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
     app.config.update(
-        CELERY_BROKER_URL='redis://redis:6379/0',
-        CELERY_RESULT_BACKEND='redis://redis:6379/0',
+        CELERY_BROKER_URL=os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0'),
+        CELERY_RESULT_BACKEND=os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0'),
     )
 
-    with app.app_context():
-        from . import main
-        app.register_blueprint(main.bp)
+    from .main import bp as main_bp
+    app.register_blueprint(main_bp)
 
     return app
